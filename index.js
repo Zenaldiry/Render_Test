d4071e96677b1083ab4d6098917a134c63855bab
zenaldiry <zenaldiry7@gmail.com>
2025-07-08 13:29:49 -0400

    edited code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.use(express.static('./dist'));
 const cors = require('cors');
 const morgan = require('morgan');
 app.use(express.json());
-app.use(cors());
+app.use(cors({}));
 
 let persons = [
   {
